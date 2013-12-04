a3febe93a8e31816bf2cce6219f64521d22e7c09
prazgaitis <prazgaitis@gmail.com>
2013-12-04 16:57:23 -0500

    added custom routes for login, timeline, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Treebook::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+
+  end
+
   resources :statuses
+  get 'timeline', to: 'statuses#index', as: :timeline
   root to: 'statuses#index'
 
 
